544b2535eb156e5a63a1216c51abfad5a8eb976d
HoAnhKhoa <traincolab2131999v1@gmail.com>
2024-01-31 20:57:41 +0700

    format code

--- gt_and_pred.sh
+++ gt_and_pred.sh
@@ -3,30 +3,25 @@ source D:/Master/OCR_Nom/fulllow_ocr_temple/.venv/Scripts/activate
 #####################
 ## HYPER-PARAMETER ##
 #####################
-GT_ZIP='gt.zip'
-
-
-echo "#########################################"
-echo "## CONVERT PPOCRLABEL FORMAT TO CLEVAL ##"
-echo "#########################################"
 GT_LABEL_TXT='D:/Master/OCR_Nom/fulllow_ocr_temple/dataset/final_data/data_demo/demo/go/de/Label.txt'
 GT_IMG_PATH='D:/Master/OCR_Nom/fulllow_ocr_temple/dataset/final_data/data_demo/demo/go/de'
 GT_PATH='go/de_gt'
-
-python get_gt.py --img_path $GT_IMG_PATH \
-                 --label_txt $GT_LABEL_TXT \
-                 --gt_path $GT_PATH
-
-
-echo "##########################################"
-echo "## CONVERT PRED PPOCR FORMAT TO CLEVAL  ##"
-echo "##########################################"
 PRED_JSON_PATH='go/de_pred/pred.json'
 PRED_IMG_PATH='D:/Master/OCR_Nom/fulllow_ocr_temple/dataset/final_data/data_demo/demo/go/de'
 PRED_PATH='go/de_pred'
 
-mkdir -p $PRED_PATH
+echo "#########################################"
+echo "## CONVERT PPOCRLABEL FORMAT TO CLEVAL ##"
+echo "#########################################"
+mkdir -p $GT_PATH
+python get_gt.py --img_path $GT_IMG_PATH \
+                 --label_txt $GT_LABEL_TXT \
+                 --gt_path $GT_PATH
 
+echo "##########################################"
+echo "## CONVERT PRED PPOCR FORMAT TO CLEVAL  ##"
+echo "##########################################"
+mkdir -p $PRED_PATH
 python get_pred.py --img_path $PRED_IMG_PATH \
                    --json_path $PRED_JSON_PATH \
                    --pred_path $PRED_PATH
