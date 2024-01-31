5b7a5701ce8c5d5e40ec362883e99de0ca6d488c
HoAnhKhoa <traincolab2131999v1@gmail.com>
2024-01-31 22:38:23 +0700

    add code to test full flow evalate

--- eval_cleval.sh
+++ eval_cleval.sh
@@ -1,17 +1,57 @@
 source D:/Master/OCR_Nom/fulllow_ocr_temple/.venv/Scripts/activate
 
-echo "############"
-echo "## CLEVAL ##"
-echo "############"
+#####################
+## HYPER-PARAMETER ##
+#####################
+ROOT='D:/Master/OCR_Nom/fulllow_ocr_temple/dataset/final_data/data_demo/demo/go/de'
+GT_PATH='go/de_gt'
+PRED_JSON_PATH='go/de_pred/pred.json'
+PRED_PATH='go/de_pred'
+PROFILE='go/profile_demo.txt'
 
-GT_PATH='ground_truth.zip'
-PRED_PATH='prediction.zip'
-PROFILE='profile_go_de.txt'
+#####################
+## HYPER-PARAMETER ##
+#####################
 
+GT_LABEL_TXT=$ROOT'/Label.txt'
+GT_ZIP='gt.zip'
+PRED_ZIP='pred.zip'
 
-cleval -g=$GT_PATH \
-       -s=$PRED_PATH \
+echo ""
+echo "############################################"
+echo "## 1. CONVERT PPOCRLABEL FORMAT TO CLEVAL ##"
+echo "############################################"
+echo ""
+mkdir -p $GT_PATH
+python get_gt.py --img_path $ROOT \
+                 --label_txt $GT_LABEL_TXT \
+                 --gt_path $GT_PATH
+
+python zip_file.py --fd_path $GT_PATH \
+                   --zip_path $GT_ZIP
+echo ""
+echo "#############################################"
+echo "## 2. CONVERT PRED PPOCR FORMAT TO CLEVAL  ##"
+echo "#############################################"
+echo ""
+mkdir -p $PRED_PATH
+python get_pred.py --img_path $ROOT \
+                   --json_path $PRED_JSON_PATH \
+                   --pred_path $PRED_PATH
+
+python zip_file.py --fd_path $PRED_PATH \
+                   --zip_path $PRED_ZIP
+
+echo ""
+echo "###############"
+echo "## 3. CLEVAL ##"
+echo "###############"
+echo ""
+
+cleval -g="$GT_PATH/$GT_ZIP" \
+       -s="$PRED_PATH/$PRED_ZIP" \
        --E2E \
-       -v \
-       --DEBUG \
-       --PROFILE > $PROFILE
+       -v > $PROFILE
+
+echo Save to $PROFILE
+echo ===== THE END =====
