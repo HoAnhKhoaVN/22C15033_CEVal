a978573f2c7e9e0b20c3f48e9df6eb60151e6349
HoAnhKhoa <traincolab2131999v1@gmail.com>
2024-01-31 22:15:12 +0700

    complete code zip file

--- zip_file.py
+++ zip_file.py
@@ -1,8 +1,8 @@
 import os
 import zipfile
-from typing import Any, Text, List
+from typing import Text, List
 
-IMG_TYPE = ['png', 'jpg', 'jpeg']
+_TYPE = ['txt']
 
 class ZipTxtFile(object):
     def __init__(
@@ -12,43 +12,40 @@ class ZipTxtFile(object):
     ) -> None:
         self.fd_path = fd_path
         self.zip_path = zip_path
-    
+        self.lst_img_fn = self.get_image_file(os.listdir(self.fd_path))
+        os.chdir(path= self.fd_path)
+
     @staticmethod
     def get_image_file(
         lst_fn : List[Text]
     )-> List[Text]:
         return list(filter(
-            lambda x: x.split('.')[-1].lower() not in IMG_TYPE,
+            lambda x: x.split('.')[-1].lower() in _TYPE,
             lst_fn
         ))
 
     def __call__(self) -> None:
-
-        # region 1: Get list of file
-        lst_fn = os.listdir(self.fd_path)
-        lst_img_fn = self.get_image_file(lst_fn)
-        lst_img_full_path = list(map(lambda x: os.path.join(self.fd_path, x), lst_img_fn))
-        print(f'lst_img_full_path: {lst_img_full_path}')
-
-        # endregion
-
-        # region 2: Zip file
         with zipfile.ZipFile(self.zip_path, 'w') as zip:
-            for file in lst_img_full_path:
+            for file in self.lst_img_fn:
                 zip.write(
                     filename=file,
                     compress_type= zipfile.ZIP_DEFLATED
                 )
-
-        # endregion
         
-
-
 if __name__ == "__main__":
-    FD_PATH = 'go/de'
+    FD_PATH = 'go/de_gt'
     obj = ZipTxtFile(
         fd_path = FD_PATH,
-        zip_path= 'demo.zip'
+        zip_path= 'gt.zip'
     )
 
     obj()
+
+
+    # FD_PATH = 'go/de_pred'
+    # obj = ZipTxtFile(
+    #     fd_path = FD_PATH,
+    #     zip_path= 'pred.zip'
+    # )
+
+    # obj()
