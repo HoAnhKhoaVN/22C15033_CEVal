fdaaa256eb580e5ae75a1d9fd95248b076cc0cbc
HoAnhKhoa <traincolab2131999v1@gmail.com>
2024-02-02 18:06:54 +0700

    add code to evaluate images with gg API

---
+++ eval_gg_api.sh
@@ -0,0 +1,57 @@
+source D:/Master/OCR_Nom/fulllow_ocr_temple/.venv/Scripts/activate
+
+#####################
+## HYPER-PARAMETER ##
+#####################
+ROOT='D:/Master/OCR_Nom/fulllow_ocr_temple/dataset/final_data/data_demo/demo/go/de'
+GT_PATH='go/de/de_gt'
+PRED_PATH='go/de/de_gg_api'
+PRED_JSON_PATH='go/de/de_gg_api/pred.json'
+PROFILE='go/de/profile_det.txt'
+
+#####################
+## HYPER-PARAMETER ##
+#####################
+
+GT_LABEL_TXT=$ROOT'/Label.txt'
+GT_ZIP='gt.zip'
+PRED_ZIP='pred.zip'
+
+echo ""
+echo "############################################"
+echo "## 1. CONVERT PPOCRLABEL FORMAT TO CLEVAL ##"
+echo "############################################"
+echo ""
+mkdir -p $GT_PATH
+python get_gt.py --img_path $ROOT \
+                 --label_txt $GT_LABEL_TXT \
+                 --gt_path $GT_PATH
+
+python zip_file.py --fd_path $GT_PATH \
+                   --zip_path $GT_ZIP
+echo ""
+echo "#############################################"
+echo "## 2. CONVERT PRED PPOCR FORMAT TO CLEVAL  ##"
+echo "#############################################"
+echo ""
+mkdir -p $PRED_PATH
+python predict/call_gg_api.py --img_path $ROOT \
+                   --json_path $PRED_JSON_PATH \
+                   --pred_path $PRED_PATH
+
+python zip_file.py --fd_path $PRED_PATH \
+                   --zip_path $PRED_ZIP
+
+echo ""
+echo "###############"
+echo "## 3. CLEVAL ##"
+echo "###############"
+echo ""
+
+cleval -g="$GT_PATH/$GT_ZIP" \
+       -s="$PRED_PATH/$PRED_ZIP" \
+       --E2E \
+       -v > $PROFILE
+
+echo Save to $PROFILE
+echo ===== THE END =====
--- predict/call_gg_api.py
+++ predict/call_gg_api.py
@@ -3,6 +3,7 @@ from google.cloud import vision
 from typing import Text, List, Dict
 from pred_ppocr_baseline import PredictionPPOCR
 import argparse
+from re import findall
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'key/apikey.json'
 os.environ['GRPC_DNS_RESOLVER'] = 'native'
@@ -26,6 +27,15 @@ class PredictionGGVision(PredictionPPOCR):
             print(f'**** INIT *****')
             print(f'image_path: {image_path}')
             print(f'json_name: {json_name}')
+
+    @staticmethod
+    def is_han_nom(
+        text: Text
+    )-> bool:
+        '''
+        Hint: https://stackoverflow.com/questions/34587346/python-check-if-a-string-contains-chinese-character
+        '''
+        return len(findall(r'[\u4e00-\u9fff]+', text)) != 0
 
     def ocr(
         self,
@@ -72,16 +82,19 @@ class PredictionGGVision(PredictionPPOCR):
         # region 5: Convert to dictionary
         res = []
         for text, b in zip(description_text, bounds):
+            if not self.is_han_nom(text):
+                continue
             bbox = [
-                [b[0].x, b[0].y],
-                [b[1].x, b[1].y],
-                [b[2].x, b[2].y],
-                [b[3].x, b[3].y]
+                [b.vertices[0].x, b.vertices[0].y],
+                [b.vertices[1].x, b.vertices[1].y],
+                [b.vertices[2].x, b.vertices[2].y],
+                [b.vertices[3].x, b.vertices[3].y]
             ]
             res.append(
                 {
                 'transcription': text,
                 'points': bbox,
+                'prob': 0.99
             }
         )
 
@@ -92,33 +105,39 @@ class PredictionGGVision(PredictionPPOCR):
         return res
 
 if __name__ == '__main__':
-    # region get argument
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-ip', 
-        '--img_path', 
-        help="Path to folder contain images"
-    )
-    parser.add_argument(
-        '-lb', 
-        '--json_path',  
-        help="Path to json"
-    )
+    # # region get argument
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument(
+    #     '-ip', 
+    #     '--img_path', 
+    #     help="Path to folder contain images"
+    # )
+    # parser.add_argument(
+    #     '-lb', 
+    #     '--json_path',  
+    #     help="Path to json"
+    # )
 
-    parser.add_argument(
-        '-gt', 
-        '--pred_path',  
-        help="Path to predicto truth"
-    )
+    # parser.add_argument(
+    #     '-gt', 
+    #     '--pred_path',  
+    #     help="Path to predicto truth"
+    # )
 
-    args = parser.parse_args()
-    # endregon
+    # args = parser.parse_args()
+    # # endregon
 
+    # PredictionGGVision(
+    #     image_path= args.img_path,
+    #     json_name= args.json_path,
+    #     debug = False
+    # ).convert_to_format_mAP(path = args.pred_path)
+
+    img_path = "D:/Master/OCR_Nom/fulllow_ocr_temple/dataset/final_data/data_demo/demo/go/de"
+    json_path = "go/de/de_gg_api/pred.json"
+    pred_path = "go/de/de_gg_api"
     PredictionGGVision(
-        image_path= args.img_path,
-        json_name= args.json_path,
+        image_path= img_path,
+        json_name= json_path,
         debug = False
-    ).convert_to_format_mAP(
-        path = args.pred_path
-    )
-
+    ).convert_to_format_mAP(path = pred_path)
