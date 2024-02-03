bf14de2efe130763dae29bc2926aede9adf4cf72
HoAnhKhoa <traincolab2131999v1@gmail.com>
2024-02-03 07:17:53 +0700

    add setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+# Src: https://youtu.be/Mgp6-ZMEcE0?si=ihkm3YejaIOKiUlg
+from setuptools import setup, find_packages
+
+setup(
+    name = "khoa_eval",
+    version= "0.2.1",
+    packages= find_packages()
+)
